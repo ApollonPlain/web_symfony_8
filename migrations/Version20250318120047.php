401091ac2175d3333765ede19f71af5d07cedff5
Guillaume GAUVAIN <guillaume.gauvain.conseil@gmail.com>
2025-12-02 19:41:55 +0100

    fix: migration modfiy with table never exist

--- migrations/Version20250318120047.php
+++ migrations/Version20250318120047.php
@@ -20,12 +20,10 @@ final class Version20250318120047 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE request_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE daily_quiz_progress_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE daily_quiz_progress (id INT NOT NULL, user_id INT NOT NULL, date DATE NOT NULL, total_answered INT NOT NULL, correct_answers INT NOT NULL, daily_goal INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_264552B5A76ED395 ON daily_quiz_progress (user_id)');
         $this->addSql('ALTER TABLE daily_quiz_progress ADD CONSTRAINT FK_264552B5A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('DROP TABLE request');
     }
 
     public function down(Schema $schema): void
